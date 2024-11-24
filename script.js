60aff20db3e7550ef12895cc58474318688f84c6
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2024-11-24 12:26:25 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const loading = document.getElementById("loading");
+    const content = document.getElementById("content");
+    setTimeout(() => {
+        loading.style.display = "none";  
+    }, 2000);
+});
